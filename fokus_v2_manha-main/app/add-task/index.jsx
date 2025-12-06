0efef7c3dd7c2e8621cd711a12d892a189bd29c2
Laura Fagundes <lauraf4gundes@gmail.com>
2025-12-06 09:11:58 -0300

    Delete fokus_v2_manha-main directory

--- fokus_v2_manha-main/app/add-task/index.jsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function AddTask() {
-    return(
-        <View>
-            <Text> Página para adicionar tarefa </Text>
-        </View>
-    )
-}
